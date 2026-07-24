93b3654b306d29bb4611232460f1852948765ab6
Alim <alimuzzamanprince@gmail.com>
2026-07-25 02:00:30 +0600

    Add React frontend with initial portfolio homepage

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>M. Alimuzzaman</h1>
+      <p>PMP® Certified Project Manager</p>
+    </main>
+  )
+}
+
+export default App
